563a3e7a32f8ac4d930c45a46f786f8237808aef
ackers93 <andrew.ackers@gmail.com>
2025-12-19 22:20:54 -0800

    aligning models and controllers with updates

--- app/models/attendee_registration.rb
+++ app/models/attendee_registration.rb
@@ -7,7 +7,9 @@ class AttendeeRegistration < ApplicationRecord
   validates :guardian_1_name, :guardian_1_email, :guardian_1_phone, presence: true
   validates :guardian_1_address_line_1, :guardian_1_city, :guardian_1_state, :guardian_1_zip, presence: true
   validates :guardian_1_email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :emergency_contact_1_name, :emergency_contact_1_phone, presence: true
   validates :terms_agreement, acceptance: true
+  validates :medical_consent, acceptance: true
 
   # Validate at least one attendee
   validate :must_have_at_least_one_attendee
