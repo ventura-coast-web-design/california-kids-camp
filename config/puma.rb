9a57baf37576160020b9193971dc5008ebd50527
ackers93 <andrew.ackers@gmail.com>
2026-01-06 14:45:48 +1300

    deploying on fly.io

--- config/puma.rb
+++ config/puma.rb
@@ -29,7 +29,9 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+# Bind to 0.0.0.0 to accept connections from all interfaces (required for Fly.io)
+# Use bind instead of port to ensure it listens on all interfaces
+bind "tcp://0.0.0.0:#{ENV.fetch("PORT", 3000)}"
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
